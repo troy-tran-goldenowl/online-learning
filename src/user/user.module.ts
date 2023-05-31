d1e0e48501ec46eaaf387e645c471405eff74adf
Troy Tran <troy.tran.goldenowl@gmail.com>
2023-05-31 15:09:54 +0700

    chore: remove user controller

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,11 +1,9 @@
 import { Module } from '@nestjs/common';
-import { UserController } from './user.controller';
 import { UserService } from './user.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 
 @Module({
-  controllers: [UserController],
   providers: [UserService],
   imports: [TypeOrmModule.forFeature([User])],
   exports: [UserService],
