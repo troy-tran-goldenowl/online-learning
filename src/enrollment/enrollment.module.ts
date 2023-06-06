0bb0cd6d8cfeece9166fc5b302fecce94a3f5548
Troy Tran <troy.tran.goldenowl@gmail.com>
2023-06-06 15:19:08 +0700

    fix: add course not found error

--- src/enrollment/enrollment.module.ts
+++ src/enrollment/enrollment.module.ts
@@ -3,11 +3,12 @@ import { EnrollmentController } from './enrollment.controller';
 import { EnrollmentService } from './enrollment.service';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { Enrollment } from './entities/enrollment.entity';
+import { CourseModule } from 'src/course/course.module';
 
 @Module({
   controllers: [EnrollmentController],
   providers: [EnrollmentService],
-  imports: [TypeOrmModule.forFeature([Enrollment])],
+  imports: [TypeOrmModule.forFeature([Enrollment]), CourseModule],
   exports: [EnrollmentService],
 })
 export class EnrollmentModule {}
--- src/enrollment/enrollment.service.ts
+++ src/enrollment/enrollment.service.ts
@@ -1,14 +1,20 @@
-import { ConflictException, Injectable } from '@nestjs/common';
+import {
+  ConflictException,
+  Injectable,
+  NotFoundException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { Enrollment } from './entities/enrollment.entity';
 import { Repository } from 'typeorm';
 import { CreateEnrollmentDto } from './dtos/create-enrollment.dto';
+import { CourseService } from 'src/course/course.service';
 
 @Injectable()
 export class EnrollmentService {
   constructor(
     @InjectRepository(Enrollment)
     private readonly enrollmentRepository: Repository<Enrollment>,
+    private readonly courseService: CourseService,
   ) {}
 
   findOne(userId: number, courseId: number) {
@@ -19,10 +25,19 @@ export class EnrollmentService {
   }
 
   async create(createEnrollmentDto: CreateEnrollmentDto, userId: number) {
+    const course = await this.courseService.findOne(
+      createEnrollmentDto.courseId,
+    );
+
+    if (!course) {
+      throw new NotFoundException('Course not found');
+    }
+
     const isExits = await this.findOne(userId, createEnrollmentDto.courseId);
     if (isExits) {
       throw new ConflictException('User is already enrolled');
     }
+
     const enrollment = this.enrollmentRepository.create({
       user: { id: userId },
       course: { id: createEnrollmentDto.courseId },
