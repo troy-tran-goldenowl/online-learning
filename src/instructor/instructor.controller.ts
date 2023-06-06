d3bb93991ac598f30ca772e21ccc2685d2d6e316
Troy Tran <troy.tran.goldenowl@gmail.com>
2023-06-06 15:03:47 +0700

    feat: apply pagination for get all course by instructor enpoint

--- src/instructor/instructor.controller.ts
+++ src/instructor/instructor.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Post, UseGuards } from '@nestjs/common';
+import {
+  Controller,
+  DefaultValuePipe,
+  Get,
+  ParseIntPipe,
+  Post,
+  Query,
+  UseGuards,
+} from '@nestjs/common';
 import { CurrentUser } from 'src/auth/decorators/current-user.decorator';
 import { InstructorService } from './instructor.service';
 import { JwtAuthGuard } from 'src/auth/guards/jwt-auth.guard';
@@ -21,7 +29,15 @@ export class InstructorController {
 
   @UseGuards(InstructorGuard)
   @Get('courses')
-  getAllCourses(@CurrentUser() user: User) {
-    return this.courseInstructorService.findAllCourseByUserId(user.id);
+  getAllCourses(
+    @CurrentUser() user: User,
+    @Query('page', new DefaultValuePipe(1), ParseIntPipe) page = 1,
+    @Query('limit', new DefaultValuePipe(10), ParseIntPipe) limit = 10,
+  ) {
+    return this.courseInstructorService.findAllCourseByUserId(
+      user.id,
+      page,
+      limit,
+    );
   }
 }
