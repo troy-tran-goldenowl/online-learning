c3331811e7f48c05eeb0f244644ca224418f8845
Troy Tran <troy.tran.goldenowl@gmail.com>
2023-06-07 10:40:24 +0700

    fix: apply global pipe config for create lesson dto

--- src/lesson/dtos/create-lesson.dto.ts
+++ src/lesson/dtos/create-lesson.dto.ts
@@ -1,9 +1,6 @@
-import { Type } from 'class-transformer';
 import { IsInt, IsNumber, IsString, Min } from 'class-validator';
 
 export class CreateLessonDto {
-  // The data is sent from form data is default string so we need to convert it to integer
-  @Type(() => Number)
   @IsInt()
   courseId: number;
 
@@ -13,12 +10,10 @@ export class CreateLessonDto {
   @IsString()
   content: string;
 
-  @Type(() => Number)
   @IsInt()
   @Min(0)
   durationInMinutes: number;
 
-  @Type(() => Number)
   @IsNumber()
   @Min(0)
   order: number;
--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,16 @@ import { ValidationPipe, VersioningType } from '@nestjs/common';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
 
-  app.useGlobalPipes(new ValidationPipe({ whitelist: true }));
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+      transformOptions: {
+        enableImplicitConversion: true,
+      },
+    }),
+  );
+
   app.setGlobalPrefix('api');
   app.enableVersioning({
     type: VersioningType.URI,
